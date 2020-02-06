3ea250103e4ec51ece4b4084a5351868ec608bf7
Stephen Riley <jstephen.riley@yahoo.com>
2020-02-06 00:13:44 -0800

    added an advanced mode

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # cyber-effect
 
+## Cyber
+
 Anything marked with class `.cybertext` will change color (black toward white) in response to an associated audio track's spectrogram.
 
 The color of the text is reflective of the average of the frequency response at the moment (every 1/100th of a second in this example).
+
+## Advanced
+
+In this case, the inner text in the specified element is treated as the spectrum of the sound; that is, the first character represents the lowest frequencies, and the last character represents the highest.  The text is split into individual `<span>`s whose `color` style is set to a dynamically generated CSS var like `--ca-0-9`.  The lambda invoked by `setInterval()` then updates the CSS vars according to the frequency response spectrum at that moment in time.
+
+## The color spectrum
+
+The colors stretch across the following colors:
+
+* 128,0,0 -- dark red
+* 255,0,0 -- red
+* 255,168,0 -- orange
+* 255,255,0 -- yellow
+* 255,255,255 -- white
+
+By adjusting the `red_pts`, `green_pts`, and `blue_pts` arrays, you can shift how much each color is in the spectrum.  The `build_colors( max )` function stretches those colors out across the `red`, `green`, and `blue` arrays from `0` to `max`.  You should probably leave `max=255`.
--- cyber-effect.js
+++ cyber-effect.js
@@ -1,6 +1,8 @@
 let audio, context, buffer, cssvar, analyzer;
 
 function cyber_init(varname, audio_file) {
+    build_colors(255);
+
     cssvar = varname;
 
     audio = new Audio();
@@ -29,7 +31,80 @@ function update() {
         sum += buffer[i];
     }
 
-    avg = Math.round(sum / analyser.frequencyBinCount) * 2;
+    avg = Math.min(255, Math.round(sum / analyser.frequencyBinCount) * 2);
 
-    document.documentElement.style.setProperty(cssvar, `rgb(${avg},${avg},${avg})`);
+    document.documentElement.style.setProperty(cssvar, `rgb(${red[avg]},${green[avg]},${blue[avg]})`);
+}
+
+
+let advancedCount = -1;
+
+function cyber_init_advanced(element_id, audio_file, spectrumPercent) {
+    advancedCount++;
+
+    build_colors(255);
+
+    const audio = new Audio();
+    const context = new (window.AudioContext || window.webkitAudioContext);
+    const analyser = context.createAnalyser();
+
+    audio.src = audio_file;
+    const source = context.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(context.destination);
+
+    const buffer = new Uint8Array(analyser.frequencyBinCount);
+
+    const text = document.getElementById(element_id).innerText;
+    if (text.length > analyser.frequencyBinCount) {
+        throw `text length must be <${analyser.frequencyBinCount}`;
+    }
+
+    const factor = analyser.frequencyBinCount / text.length * spectrumPercent;
+
+    const textArray = text.split('');
+    let innerHtml = '';
+    for (let i = 0; i < textArray.length; i++) {
+        const cssvar = `--ca-${advancedCount}-${i}`;
+        innerHtml += `<span style='color:var(${cssvar})'>${textArray[i]}</span>`;
+        document.documentElement.style.setProperty(cssvar, `rgb(0,0,0)`);
+    }
+    document.getElementById(element_id).innerHTML = innerHtml;
+
+    audio.play();
+
+    setInterval(() => {
+        analyser.getByteFrequencyData(buffer);
+
+        for (let i = 0; i < textArray.length; i++) {
+            const index = Math.round(factor * i);
+            const level = buffer[index];
+            const cssvar = `--ca-${advancedCount}-${i}`;
+            document.documentElement.style.setProperty(cssvar, `rgb(${red[level]},${green[level]},${blue[level]})`);
+            // console.log(`buffer[${index}] = ${level}`);
+        }
+        // console.log("");
+    }, 100);
+}
+
+const red = [], green = [], blue = [];
+const red_pts = [128, 128, 128, 128, 255, 255, 255, 255],
+    green_pts = [0, 0, 0, 0, 0, 168, 255, 255],
+    blue_pts = [0, 0, 0, 0, 0, 0, 0, 255];
+
+function build_colors(max) {
+    const factor = Math.round(max / red_pts.length);
+
+    for (let i = 0; i < red_pts.length - 1; i++) {
+        const start = Math.trunc(i * factor);
+        const end = Math.trunc((i + 1) * factor);
+        for (let j = start; j < end; j++) {
+            const r = red_pts[i] + (red_pts[i + 1] - red_pts[i]) * j / (end - start);
+            const g = green_pts[i] + (green_pts[i + 1] - green_pts[i]) * j / (end - start);
+            const b = blue_pts[i] + (blue_pts[i + 1] - blue_pts[i]) * j / (end - start);
+            red[j] = r;
+            green[j] = g;
+            blue[j] = b;
+        }
+    }
 }
